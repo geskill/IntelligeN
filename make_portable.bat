6d73681ed11a1dd4b93165e6fe5a7bc745e687ed
Sebastian Klatte <sebastianklatte@gmx.net>
2015-10-19 20:31:58 +0200

    Exit if not possible to change dir.

--- make_portable.bat
+++ make_portable.bat
@@ -4,5 +4,7 @@ set portable="%~dp0bin_portable\"
 
 cd /d %portable%
 
+if errorlevel 1 exit /b
+
 for /F "delims=" %%i in ('dir /b') do if not "%%i"=="IntelligeN.dll" (rmdir "%%i" /s/q || del "%%i" /s/q)
 
