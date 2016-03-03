734cee8d581a62a9f5cbf57d9817b20419319fae
geskill <sebastianklatte@gmx.net>
2016-03-03 19:40:49 +0100

    Added relink.to crypter plug-in.

---
+++ src/sdk/plugins/crypter/relink.to/relinkto.dpr
@@ -0,0 +1,26 @@
+﻿library relinkto;
+
+{$R *.dres}
+
+uses
+  uPlugInInterface,
+  uPlugInCrypterClass,
+  uRelinkTo in 'uRelinkTo.pas';
+
+{$R *.res}
+
+function LoadPlugin(var APlugIn: ICrypterPlugIn): WordBool; safecall; export;
+begin
+  try
+    APlugIn := TRelinkTo.Create;
+    Result := True;
+  except
+    Result := False;
+  end;
+end;
+
+exports
+  LoadPlugIn name 'LoadPlugIn';
+
+begin
+end.
