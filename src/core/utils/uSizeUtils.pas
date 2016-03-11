417e6d9e821fd1286130b241488fd90c2705efbf
geskill <sebastianklatte@gmx.net>
2016-03-11 16:30:56 +0100

    Fixed SizeToByte()-function handling ThousandSeparator properly.

--- src/core/utils/uSizeUtils.pas
+++ src/core/utils/uSizeUtils.pas
@@ -8,26 +8,34 @@ uses
 
 type
   TSizeFormatter = class
-    class function SizeToByte(ASizeString: string; ASizeType: string = ''; ADotAsDecimalSeparator: Boolean = True): Int64;
+    class function SizeToByte(const ASizeString: string; const ASizeType: string = ''; ADotAsDecimalSeparator: Boolean = True): Int64;
   end;
 
 implementation
 
 { TSizeFormatter }
 
-class function TSizeFormatter.SizeToByte(ASizeString: string; ASizeType: string = ''; ADotAsDecimalSeparator: Boolean = True): Int64;
+class function TSizeFormatter.SizeToByte(const ASizeString: string; const ASizeType: string = ''; ADotAsDecimalSeparator: Boolean = True): Int64;
 var
   LFormatSettings: TFormatSettings;
+  LSizeString: string;
   LSize: Extended;
 begin
+  // TextToFloat ignores ThousandSeparator
+  // see: http://qc.embarcadero.com/wc/qcmain.aspx?d=92265
   GetLocaleFormatSettings(LOCALE_USER_DEFAULT, LFormatSettings);
   if ADotAsDecimalSeparator then
-    LFormatSettings.DecimalSeparator := '.'
+  begin
+    LFormatSettings.DecimalSeparator := '.';
+    LSizeString := StringReplace(ASizeString, ',', '', [rfReplaceAll]);
+  end
   else
+  begin
     LFormatSettings.DecimalSeparator := ',';
+    LSizeString := StringReplace(ASizeString, '.', '', [rfReplaceAll]);
+  end;
 
-  LSize := StrToFloatDef(ASizeString, 0, LFormatSettings);
-
+  LSize := StrToFloatDef(LSizeString, 0, LFormatSettings);
   if (Pos('KB', UpperCase(ASizeType)) > 0) then
     Result := Round(LSize * 1024)
   else if (Pos('MB', UpperCase(ASizeType)) > 0) then
