a0bac3a44b12baccd1e664872be12745aa07d069
geskill <sebastianklatte@gmx.net>
2016-08-31 12:27:06 +0200

    Updated share-links.biz crypter plugin status check.

--- src/sdk/plugins/crypter/share-links.biz/uShareLinksBiz.pas
+++ src/sdk/plugins/crypter/share-links.biz/uShareLinksBiz.pas
@@ -12,7 +12,9 @@ uses
   // HTTPManager
   uHTTPInterface, uHTTPClasses,
   // plugin system
-  uPlugInCrypterClass, uPlugInHTTPClasses, uPlugInConst;
+  uPlugInCrypterClass, uPlugInHTTPClasses, uPlugInConst,
+  // Utils
+  uPathUtils, uStringUtils, uURLUtils;
 
 type
   TShareLinksBiz = class(TCrypterPlugIn)
@@ -156,6 +158,10 @@ var
   LHTTPParams: IHTTPParams;
   LRequestID: Double;
   LHTTPProcess: IHTTPProcess;
+
+  LCompleteList, LSingleList: TStrings;
+  LSizeInBytes, LPartSizeInBytes: Int64;
+  I, unknown, online, offline: Integer;
 begin
   Result := False;
 
@@ -170,6 +176,12 @@ begin
     StatusImage := '';
     StatusImageText := '';
   end;
+
+  LSizeInBytes := 0;
+  LPartSizeInBytes := 0;
+  unknown := 0;
+  online := 0;
+  offline := 0;
 
   LHTTPParams := THTTPParams.Create;
   with LHTTPParams do
@@ -192,48 +204,72 @@ begin
   begin
     ErrorMsg := LHTTPProcess.HTTPResult.HTTPResponseInfo.ErrorMessage;
   end
-  else if (Pos('***', string(LHTTPProcess.HTTPResult.SourceCode)) = 0) then
+  else if Pos(';', LHTTPProcess.HTTPResult.SourceCode) > 0 then
   begin
-    with TRegExpr.Create do
-      try
-        InputString := LHTTPProcess.HTTPResult.SourceCode;
-        Expression := '(.*?);(.*?);(\d+);(.*?);(\d+);(.*?)';
-
-        if Exec(InputString) then
+    LCompleteList := TStringList.Create;
+    try
+      LCompleteList.Text := LHTTPProcess.HTTPResult.SourceCode;
+      for I := 0 to LCompleteList.Count - 1 do
+      begin
+        if not(LCompleteList[I] = '') then
         begin
-          // TODO: Validate this
-
-          case IndexText(Match[6], ['online', 'unknown', 'offline', 'unchecked', 'parts']) of
-            0:
-              ACrypterFolderInfo.Status := csOnline;
-            1:
-              ACrypterFolderInfo.Status := csUnknown;
-            2:
-              ACrypterFolderInfo.Status := csOffline;
-            3:
-              ACrypterFolderInfo.Status := csMixedOnOffline;
-          else
-            ACrypterFolderInfo.Status := csUnknown;
+          Inc(ACrypterFolderInfo.Parts);
+          LSingleList := SplittString(';', LCompleteList[I]);
+          try
+            try
+              case IndexText(LSingleList[5], ['2', '1', '0']) of
+                0:
+                  Inc(unknown);
+                1:
+                  Inc(online);
+                2:
+                  Inc(offline);
+              end;
+              if StrToIntDef(LSingleList[2], 0) > LPartSizeInBytes then
+              begin
+                LPartSizeInBytes := StrToIntDef(LSingleList[2], 0);
+              end;
+              LSizeInBytes := LSizeInBytes + StrToIntDef(LSingleList[2], 0);
+              ACrypterFolderInfo.Hoster := LSingleList[3];
+            except
+              on E: Exception do
+              begin
+                ErrorMsg := 'The result from ' + GetName + ' was invaild: ' + E.message;
+              end;
+            end;
+          finally
+            LSingleList.Free;
           end;
-
-          ACrypterFolderInfo.Size := RoundTo((StrToInt64(Match[3]) / 1048576), -2);
-          ACrypterFolderInfo.Hoster := Match[4];
-          ACrypterFolderInfo.Parts := 0;
-
-          Result := True;
-        end
-        else
-        begin
-          Self.ErrorMsg := LHTTPProcess.HTTPResult.SourceCode;
         end;
-      finally
-        Free;
       end;
+    finally
+      LCompleteList.Free;
+    end;
+    if (unknown = 0) and (online = 0) then
+      ACrypterFolderInfo.Status := csOffline
+    else if (unknown = 0) and (offline = 0) then
+      ACrypterFolderInfo.Status := csOnline
+    else if (offline > 0) and (online > 0) then
+      ACrypterFolderInfo.Status := csMixedOnOffline
+    else
+      ACrypterFolderInfo.Status := csUnknown;
+
+    ACrypterFolderInfo.Link := AFolderIdentifier;
+    ACrypterFolderInfo.Size := RoundTo((LSizeInBytes / 1048576), -2);
+    ACrypterFolderInfo.PartSize := RoundTo((LPartSizeInBytes / 1048576), -2);
+
+    Result := True;
   end
   else
   begin
     ErrorMsg := Trim(LHTTPProcess.HTTPResult.SourceCode);
   end;
+
+  {
+  [ API-RESPONSE ]
+    CSV-Output   URL;filename;filesize;provider_shortcut;backup_number;status
+                 0   1        2        3                 4             5
+  }
 end;
 
 end.
